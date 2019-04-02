c5dc98ac5537a56543cf7ee4afb400fc171c025c
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-04-02 15:44:43 -0700

    change firebase class to encap user obj

--- frame/app/main/events.py
+++ frame/app/main/events.py
@@ -9,7 +9,7 @@ from . import fb
 @socketio.on('connect')
 def socket_connect():
 	# images = ['pic_1.jpg', 'pic_2.jpg', 'pic_4.jpg', 'small_pic_1.jpg', 'small_pic_2.jpg', 'small_pic_3.jpg']
-	images = fb.show_all(fb.user);
+	images = fb.show_all();
 	print(images);
 	emit('initialize', json.dumps(images), namespace='/')
 	print('connection established')
