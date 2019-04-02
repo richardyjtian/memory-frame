7a98581bf5e5a8588ff3670150529c16412b62f7
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-04-02 15:44:43 -0700

    change firebase class to encap user obj

--- frame/app/main/firebase.py
+++ frame/app/main/firebase.py
@@ -10,9 +10,8 @@ class Firebase:
 
     def sign_in(self, email, psw):
         self.user = self.auth.sign_in_with_email_and_password(email, psw)
-        return self.user
     
-    def filter(self, key, val, user):
+    def filter(self, key, val):
         """query the database: select url from user_photos where key = val
         Args:
             key: name of the field in a database entry 
@@ -24,19 +23,19 @@ class Firebase:
         eg. user = firebase.sign_in('user0@gmail.com', '123123')
             empty_name = firebase.filter('name', '',user)
         """
-        all_val = self.database.child(user.get('localId')).order_by_child(key).equal_to(val).get(token = user.get('idToken')).val()
+        all_val = self.database.child(self.user.get('localId')).order_by_child(key).equal_to(val).get(token = self.user.get('idToken')).val()
         result = []
         for val in all_val:
-          result.append(self.database.child(user.get('localId')).child(val).get(token = user.get('idToken')).val().get("imageUrl"))
+          result.append(self.database.child(self.user.get('localId')).child(val).get(token = self.user.get('idToken')).val().get("imageUrl"))
         return result
 
-    def show_all(self, user):
+    def show_all(self):
         """ All pictures under the account of the given user
         Returns an array of URLs to the pictures
         """
-        all_val = self.database.child(user.get('localId')).get(token = user.get('idToken')).val()
+        all_val = self.database.child(self.user.get('localId')).get(token = self.user.get('idToken')).val()
         result = []
         for val in all_val:
-            result.append(self.database.child(user.get('localId')).child(val).get(token = user.get('idToken')).val().get("imageUrl"))
+            result.append(self.database.child(self.user.get('localId')).child(val).get(token = self.user.get('idToken')).val().get("imageUrl"))
         return result
     
