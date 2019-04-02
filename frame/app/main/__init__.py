b9033b278adc2eebe717c5ffcba7101cc7ba2b27
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-04-01 22:53:09 -0700

    firebase init integration

--- frame/app/main/__init__.py
+++ frame/app/main/__init__.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 from flask_ask import Ask
+from . import firebase
+from . import config
 
 main = Blueprint('main', __name__, url_prefix="/")
 ask = Ask(blueprint=main)
+fb = firebase.Firebase(config.firebase_config)
+fb.sign_in(config.user_config['email'], config.user_config['password'])
 
 from . import routes, events, intents
