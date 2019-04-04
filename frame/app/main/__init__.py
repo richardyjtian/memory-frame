4c98cca8b90c7da141e6a47eec8f25dd9727be1a
Skylar <skylarliang233@gmail.com>
2019-04-04 13:28:00 -0700

    check if already has a user

--- frame/app/main/__init__.py
+++ frame/app/main/__init__.py
@@ -7,10 +7,10 @@ from bluetooth import bluetooth
 main = Blueprint('main', __name__, url_prefix="/")
 ask = Ask(blueprint=main)
 fb = firebase.Firebase(config.firebase_config)
-email, psw = bluetooth()
-fb.sign_in(email, psw)
-config.user_config['email'] = email
-config.user_config['password'] = psw
-#fb.sign_in(config.user_config['email'], config.user_config['password'])
+if not config.user_config['email']:
+    email, psw = bluetooth()
+    config.user_config['email'] = email
+    config.user_config['password'] = psw
+fb.sign_in(config.user_config['email'], config.user_config['password'])
 
 from . import routes, events, intents
